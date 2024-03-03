5a4fc537060dcb43caacaaf6d6a25dc033f446a6
AkshatJha21 <akshatj2134@gmail.com>
2024-03-03 22:13:03 +0530

    express task

---
+++ Class-4/express/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+
+var users = [
+    {
+        name: "Akshat Jha",
+        kidneys: [
+            { 
+                healthy: true
+            },
+        ]
+    },
+]
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// CHECK HEALTH OF KIDNEY(S)
+app.get("/", function(req, res) {
+    const kidneys = users[0].kidneys;
+    const numOfKidneys = kidneys.length;
+    let healthyKidneys = 0;
+    for (let i = 0; i < numOfKidneys; i++) {
+        if (kidneys[i].healthy) {
+            healthyKidneys++;
+        }
+    };
+    const unhealthyKidneys = numOfKidneys - healthyKidneys;
+
+    res.json({
+        numOfKidneys,
+        healthyKidneys,
+        unhealthyKidneys
+    });
+});
+
+app.post("/", function(req, res) {
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    });
+    res.json({
+        msg: "Done"
+    })
+});
+
+app.listen(port, function(req, res) {
+    console.log("Listening on port ", port);
+})
