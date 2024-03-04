200789dc11f9a8d8ade2476d3d4ea5272f8e7974
AkshatJha21 <akshatj2134@gmail.com>
2024-03-04 19:48:21 +0530

    fileServer complete

--- trial-code/week-2/node/fileServer.js
+++ trial-code/week-2/node/fileServer.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
 
-// npm install express
-// search 'path'
+const app = express();
+const port = 3000;
+
+const filePath = './files/';
+
+function listFiles(path) {
+    const files = fs.readdirSync(path);
+    return files;
+}
+
+app.get('/files', function(req, res) {
+    const files = listFiles(filePath);
+    res.json({files}).status(200);
+});
+
+app.get('/files/:filename', function(req, res) {
+    const filename = req.params.filename;
+    let dir = filePath + filename
+    fs.readFile(dir, "utf-8", function(err, data) {
+        if (err) {
+            res.status(404).send('File not found');
+        }
+        res.send(data).status(200);
+    });
+});
+
+app.get('*', function(req, res) {
+    res.status(404).send('404 - Not Found')
+});
+
+app.listen(port, function(req, res) {
+    console.log("Listening on port " + port);
+});
