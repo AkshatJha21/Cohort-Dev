19d9aa92c8f7fdf255848045eab386f3fd62baef
AkshatJha21 <akshatj2134@gmail.com>
2024-03-05 13:37:09 +0530

    todoServer get routes complete

--- trial-code/week-2/node/todoServer.js
+++ trial-code/week-2/node/todoServer.js
@@ -3,6 +3,32 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+var todos = [
+    {
+        title: "Do task 1",
+        description: "Complete task 1 before 6pm"
+    },
+];
+
+function newTodo(title, description) {
+    todos.push({title, description});
+};
+
+app.get('/todos', function(req, res) {
+    res.json(todos).status(200);
+});
+
+app.get('/todos/:id', function(req, res) {
+    const id = req.params.id;
+    if (id < 0 || id >= todos.length) {
+        res.send('Todo not found (id is out of range)').status(404);
+    }
+    const todoById = todos[id];
+    res.json(todoById).status(200);
+});
+
+newTodo("Make food", "Finish before 10pm");
+
 app.listen(port, function(req, res) {
     console.log('Listening on port ' + port);
 });
