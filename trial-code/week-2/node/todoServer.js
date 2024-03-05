c34c30fa303130a0dc07f333c0650c7962c19a99
AkshatJha21 <akshatj2134@gmail.com>
2024-03-05 14:24:15 +0530

    post request complete

--- trial-code/week-2/node/todoServer.js
+++ trial-code/week-2/node/todoServer.js
@@ -1,17 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 var todos = [
     {
         title: "Do task 1",
-        description: "Complete task 1 before 6pm"
+        description: "Complete task 1 before 6pm",
+        completed: true
     },
+    {
+        title: "Do task 2",
+        description: "Do not complete immediately",
+        completed: false
+    }
 ];
 
-function newTodo(title, description) {
-    todos.push({title, description});
+function newTodo(title, description, completed) {
+    todos.push({title, description, completed});
+};
+
+function updateTodo(id, key, value){
+    const todo = todos[id];
+    todo.key = value;
 };
 
 app.get('/todos', function(req, res) {
@@ -21,13 +35,23 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
     const id = req.params.id;
     if (id < 0 || id >= todos.length) {
-        res.send('Todo not found (id is out of range)').status(404);
+        res.status(404).send('Todo not found (id is out of range)');
     }
     const todoById = todos[id];
-    res.json(todoById).status(200);
+    res.status(200).json(todoById);
 });
 
-newTodo("Make food", "Finish before 10pm");
+app.post('/todos', (req, res) => {
+    const body = req.body;
+    newTodo(body.title, body.description, body.completed);
+    const newTodoId = todos.length - 1;
+    res.status(201).send(`Created with id: ${newTodoId}`);
+});
+
+app.put('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    updateTodo()
+});
 
 app.listen(port, function(req, res) {
     console.log('Listening on port ' + port);
