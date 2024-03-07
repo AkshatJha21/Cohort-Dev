013584478aaea4008dda8f8c9aa0f8e3bf10a06c
AkshatJha21 <akshatj2134@gmail.com>
2024-03-07 19:40:07 +0530

    middleware and zod done

--- Class-5/middleware/index.js
+++ Class-5/middleware/index.js
@@ -3,28 +3,38 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/health-checkup", (req, res) => {
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const kidneyId = req.query.kidneyId;
-
-    if (username != 'akshat' || password != 'passkey1') {
-        res.status(400).json({
+function userMiddleware(req, res, next) {
+    if (req.headers.username != 'akshat' || req.headers.password != 'passkey1') {
+        res.status(403).json({
             msg: "Incorrect headers"
         });
-        return;
+    } else {
+        next();
     }
-    
-    if (kidneyId != 1 && kidneyId != 2) {
+}
+
+function kidneyMiddleware(req, res, next) {
+    if (req.query.kidneyId != 1 && req.query.kidneyId != 2) {
         res.status(400).json({
             msg: "Invalid params"
         });
-        return;
+    } else {
+        next();
     }
+}
 
+app.use(userMiddleware);
+app.use(kidneyMiddleware);
+
+app.get("/health-checkup", (req, res) => {
     res.json({
         msg: "Health OK"
     });
+});
+
+//GLOBAL CATCHES
+app.use(function(err, req, res, next) {
+    res.status(500).send('Internal Server Error');
 });
 
 app.listen(port, (req, res) => {
