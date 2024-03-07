b812b72e1304e982300c8b70503f0fc25e88b52e
AkshatJha21 <akshatj2134@gmail.com>
2024-03-07 13:23:12 +0530

    new week started - errors present

---
+++ Class-5/middleware/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get("/health-checkup", (req, res) => {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = req.params.kidneyId;
+
+    if (username !== 'akshat' || password !== 'passkey1') {
+        res.status(400).json({
+            msg: "Incorrect headers"
+        });
+        return;
+    }
+    
+    if (kidneyId != 1 && kidneyId != 2) {
+        res.status(400).json({
+            msg: "Invalid params"
+        });
+        return;
+    }
+
+    res.json({
+        msg: "Health OK"
+    });
+});
+
+app.listen(port, (req, res) => {
+    console.log('Listening on port ' + port);
+});
