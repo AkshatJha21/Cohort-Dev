d7aebf0b809d54b131766a692440457aa52b94bb
AkshatJha21 <akshatj2134@gmail.com>
2024-03-09 12:21:15 +0530

    test

--- trial-code/week-3/middleware/requestCounter.js
+++ trial-code/week-3/middleware/requestCounter.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get('/', (req,res) {
+    
+})
+
 app.listen(port, (req, res) => {
     console.log('Listening on port ' + port);
 });
