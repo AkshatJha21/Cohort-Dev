8e5221c9dee3ad9290e1750f4e8685a32e386666
AkshatJha21 <akshatj2134@gmail.com>
2024-03-11 06:37:19 +0530

    requestCounter done

--- trial-code/week-3/middleware/requestCounter.js
+++ trial-code/week-3/middleware/requestCounter.js
@@ -3,9 +3,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) {
-    
-})
+let requestCount = 0;
+
+function requestCounter(req, res, next) {
+    requestCount += 1;
+    next();
+}
+
+app.use(requestCounter);
+
+app.get('/user', function(req, res) {
+    res.status(200).json({ name: 'john' });
+});
+  
+app.post('/user', function(req, res) {
+    res.status(200).json({ msg: 'created dummy user' });
+});
+  
+app.get('/requestCount', function(req, res) {
+    res.status(200).json({ requestCount });
+});
 
 app.listen(port, (req, res) => {
     console.log('Listening on port ' + port);
