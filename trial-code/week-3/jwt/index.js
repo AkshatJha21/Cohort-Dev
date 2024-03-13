1f426d14e4bd0dd21f369ef74ffd3e027c6a1dab
AkshatJha21 <akshatj2134@gmail.com>
2024-03-13 21:54:37 +0530

    incomplete assignments

---
+++ trial-code/week-3/jwt/index.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const jwtKey = "secret";
+const zod = require("zod");
+
+const schema = zod.object({
+    username: zod.string().email({ message: "Invalid email" }),
+    password: zod.string().min(6, { message: "Must be 6 or more characters." })
+});
+
+function signJwt(username, password) {
+    const checkedUsername = schema.safeParse(username);
+    const checkedPassword = schema.safeParse(password);
+
+    const data = {
+        checkedUsername,
+        checkedPassword
+    };
+
+    const token = jwt.sign(data, jwtKey);
+
+    return token;
+}
+
+function verifyJwt(token) {
+    const verified = jwt.verify(token, jwtKey);
+}
+
