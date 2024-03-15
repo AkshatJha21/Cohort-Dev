6255f2a0123d39fd0d1c57591c2bf47a98ab4d17
AkshatJha21 <akshatj2134@gmail.com>
2024-03-15 06:16:01 +0530

    jwt signature done

--- trial-code/week-3/jwt/index.js
+++ trial-code/week-3/jwt/index.js
@@ -2,23 +2,25 @@ const jwt = require("jsonwebtoken");
 const jwtKey = "secret";
 const zod = require("zod");
 
-const schema = zod.object({
-    username: zod.string().email({ message: "Invalid email" }),
-    password: zod.string().min(6, { message: "Must be 6 or more characters." })
-});
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
 
 function signJwt(username, password) {
-    const checkedUsername = schema.safeParse(username);
-    const checkedPassword = schema.safeParse(password);
+    const checkedUsername = emailSchema.safeParse(username);
+    const checkedPassword = passwordSchema.safeParse(password);
+
+    if (!checkedUsername.success || !checkedPassword.success) {
+        return null;
+    }
 
     const data = {
         checkedUsername,
         checkedPassword
-    };
+    }
 
-    const token = jwt.sign(data, jwtKey);
+    const signature = jwt.sign(data, jwtKey);
 
-    return token;
+    return signature;
 }
 
 function verifyJwt(token) {
