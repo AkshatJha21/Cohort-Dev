f86e42926f45efd74be805d7f3d5e1a024237996
AkshatJha21 <akshatj2134@gmail.com>
2024-03-29 08:24:20 +0530

    folder created

---
+++ trial-code/week-3/mongo/db/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://admin31:adminpass31@cluster0.zkhqdtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+console.log('Mongo DB connected successfully');
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
