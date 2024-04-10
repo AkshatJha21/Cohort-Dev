f5ac64b811fa75c6dcb20de842c338b261d49d2d
AkshatJha21 <akshatj2134@gmail.com>
2024-04-10 06:36:03 +0530

    Wrapper done

--- Class-10/class-10-app/src/App.jsx
+++ Class-10/class-10-app/src/App.jsx
@@ -1,65 +1,29 @@
 import { useState, memo } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-// import Header from './components/Header'
 
 let counter = 4;
 
 function App() {
-  const [title, setTitle] = useState("My name is Unga Bunga");
-  const onRandomClick = () => {
-    const rand = Math.random();
-    setTitle(`My name is ${rand}`);
-  }
-
-  const [todos, setTodos] = useState([
-    {
-      title: "Go to gym",
-      description: "At 7am",
-      id: 1
-    },
-    {
-      title: "Get grocery",
-      description: "At 7pm",
-      id: 2
-    },
-    {
-      title: "Make bed",
-      description: "At 5 am",
-      id: 3
-    }
-  ]);
-
-  const addTodo = () => {
-    setTodos([...todos, {
-      id: counter++,
-      title: Math.random(),
-      description: Math.random()
-    }])
-  }
+  
   return (
     <div>
-      <button onClick={addTodo}>Add Todo</button>
-      {todos.map((todo) => 
-        <Todo key={todo.id} title={todo.title} description={todo.description}/>
-      )}
+      <CardWrapper>
+        Hello!
+      </CardWrapper>
     </div>
   )
 }
 
-const Header = memo(function ({title}) {
+function TextComponent() {
   return <div>
-    {title}
+    Hi there
   </div>
-})
+}
 
-function Todo({title, description}) {
-  return (
-  <div>
-    <h2>{title}</h2>
-    <h4>{description}</h4>
+function CardWrapper({children}) {
+  return <div style={{margin:10, padding:10, border: "solid black 2px"}}>
+    {children}
   </div>
-)}
+}
 
 export default App
