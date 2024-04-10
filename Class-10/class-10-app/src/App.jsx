58a5f45bc4c7af59e09357f5c2441bf4513b969c
AkshatJha21 <akshatj2134@gmail.com>
2024-04-10 07:00:38 +0530

    useEffect

--- Class-10/class-10-app/src/App.jsx
+++ Class-10/class-10-app/src/App.jsx
@@ -1,29 +1,32 @@
-import { useState, memo } from 'react'
+import { useState, memo, useEffect } from 'react'
 import './App.css'
 
-let counter = 4;
-
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch('https://sum-server.100xdevs.com/todos').then(async function(res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+    }, 10000);
+  }, []);
   
   return (
-    <div>
-      <CardWrapper>
-        Hello!
-      </CardWrapper>
-    </div>
+    <>
+      {todos.map((todo) => <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
+    </>
   )
 }
 
-function TextComponent() {
-  return <div>
-    Hi there
-  </div>
-}
-
-function CardWrapper({children}) {
-  return <div style={{margin:10, padding:10, border: "solid black 2px"}}>
-    {children}
-  </div>
+function Todo({title, description}) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <h4>{description}</h4>
+    </div>
+  )
 }
 
 export default App
