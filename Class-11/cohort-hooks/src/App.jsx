aa656d745d1876c1e6b2d2a073bb2333c0c7cdfb
AkshatJha21 <akshatj2134@gmail.com>
2024-04-16 06:29:09 +0530

    first commit 16/4

---
+++ Class-11/cohort-hooks/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import axios from "axios"
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todos").then(function (res) {
+      setTodos(res.data.todos);
+    });
+  }, [])
+  
+
+  return (
+    <>
+      {todos.map((todo) => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+    </>
+  )
+}
+
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      {description}
+    </div>
+  )
+}
+
+export default App
