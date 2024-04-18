aafae55199a1bb74ee1a58f74db3c3c944b2c57f
AkshatJha21 <akshatj2134@gmail.com>
2024-04-18 12:42:54 +0530

    useMemo assignment 2 week6

--- assignments/week-6/1-use-memo/src/components/Assignment2.jsx
+++ assignments/week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 // In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
 // You will learn something new here, specifically how you have to pass more than one value in the dependency array
 
+// THIS FUNCTION GENERATES RANDOM SENTENCES USING THE ELEMENTS OF THE WORDS ARRAY
 const words = ["hi", "my", "name", "is", "for", "to", "random", "word" ];
 const TOTAL_LINES = 1000;
 const ALL_WORDS = [];
@@ -20,7 +21,12 @@ export function Assignment2() {
     const [sentences, setSentences] = useState(ALL_WORDS);
     const [filter, setFilter] = useState("");
 
-    const filteredSentences = sentences.filter(x => x.includes(filter))
+    // filter CHANGES WHEN THE USER EDITS THE INPUT
+    // sentences CHANGES EVERY TIME ALL_WORDS IS RERENDERED
+    // HENCE, BOTH filter AND sentences ARE PRESENT IN DEPENDENCY ARRAY
+    const filteredSentences = useMemo(() => {
+        return sentences.filter(x => x.includes(filter));
+    }, [filter, sentences]);
 
     return <div>
         <input type="text" onChange={(e) => {
