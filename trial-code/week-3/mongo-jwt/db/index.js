5eac654559c53bf468c782d71b0a6b9c9e20a0e8
AkshatJha21 <akshatj2134@gmail.com>
2024-05-06 07:10:51 +0530

    06.05.24

--- trial-code/week-3/mongo-jwt/db/index.js
+++ trial-code/week-3/mongo-jwt/db/index.js
@@ -1,18 +1,14 @@
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
 mongoose.connect('mongodb+srv://admin31:adminpass31@cluster0.zkhqdtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 console.log('Mongo DB connected successfully');
 
-// Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
     username: String,
     password: String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
     username: String,
     password: String,
     purchasedCourses: [{
@@ -22,7 +18,6 @@ const UserSchema = new mongoose.Schema({
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
     title: String,
     description: String,
     price: Number,
