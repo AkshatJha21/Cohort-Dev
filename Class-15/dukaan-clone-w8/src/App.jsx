db352bc853ab3c54c7a27506f4f26a9e40d1e7e1
AkshatJha21 <akshatj2134@gmail.com>
2024-05-08 08:49:36 +0530

    revenue card

--- Class-15/dukaan-clone-w8/src/App.jsx
+++ Class-15/dukaan-clone-w8/src/App.jsx
@@ -1,22 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import RevenueCard from './components/RevenueCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='flex justify-center'>
-        <div className='bg-red-500'>yo</div>
-        <div className='bg-blue-500'>yo</div>
-        <div className='bg-green-500'>yo</div>
-      </div>
-      <div className='grid grid-cols-5'>
-        <div className='bg-pink-500 col-span-2'>grid</div>
-        <div className='bg-violet-500 col-span-2'>grid</div>
-        <div className='bg-rose-500'>grid</div>
-      </div>
-    </>
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+      <RevenueCard title={"Amount pending"} amount={24423.54} orderCount={13}/>
+    </div>
   )
 }
 
