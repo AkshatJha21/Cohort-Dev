3727400f0d40bdfebec2f6d887dada8178eb7aa0
AkshatJha21 <akshatj2134@gmail.com>
2024-05-08 07:52:57 +0530

    grids

--- Class-15/dukaan-clone-w8/src/App.jsx
+++ Class-15/dukaan-clone-w8/src/App.jsx
@@ -5,11 +5,18 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex justify-center'>
-      <div style={{backgroundColor: "red"}}>yo</div>
-      <div style={{backgroundColor: "blue"}}>yo</div>
-      <div style={{backgroundColor: "green"}}>yo</div>
-    </div>
+    <>
+      <div className='flex justify-center'>
+        <div className='bg-red-500'>yo</div>
+        <div className='bg-blue-500'>yo</div>
+        <div className='bg-green-500'>yo</div>
+      </div>
+      <div className='grid grid-cols-5'>
+        <div className='bg-pink-500 col-span-2'>grid</div>
+        <div className='bg-violet-500 col-span-2'>grid</div>
+        <div className='bg-rose-500'>grid</div>
+      </div>
+    </>
   )
 }
 
