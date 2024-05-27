7b0174e86b15dbd6ff7e50cee2e3ad06729159a3
AkshatJha21 <akshatj2134@gmail.com>
2024-05-27 23:08:34 +0530

    useIsOnline, useMousePointer, useDebounce, useInterval

--- Class-16/custom-hooks/src/App.jsx
+++ Class-16/custom-hooks/src/App.jsx
@@ -1,55 +1,78 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function useTodos() {
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(true);
+// function useIsOnline() {
+//   const [online, setOnline] = useState(window.navigator.onLine);
+
+//   useEffect(() => {
+//     window.addEventListener('online', () => setOnline(true));
+//     window.addEventListener('offline', () => setOnline(false));
+//   }, []);
+
+//   return online;
+// }
+
+// function useMousePointer() {
+//   const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+
+//   const handleMouseMove = (e) => {
+//     setCoordinates({ x: e.clientX, y: e.clientY });
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener('mousemove', handleMouseMove);
+//     return () => {
+//       window.removeEventListener('mousemove', handleMouseMove);
+//     }
+//   }, []);
+
+//   return coordinates;
+// }
+
+// function useInterval(callback, delay) {
+//   useEffect(() => {
+//     const interval = setInterval(callback, delay);
+
+//     return () => clearInterval(interval);
+//   }, [callback, delay]);
+// }
+
+function useDebounce(input, interval) {
+  const [debounce, setDebounce] = useState(input);
 
   useEffect(() => {
-    // const val = setInterval(() => {
-    //   axios.get("https://sum-server.100xdevs.com/todos")
-    //     .then(res => {
-    //       setTodos(res.data.todos);
-    //       setLoading(false);
-    //     })
-    // }, n * 1000);
-    
-      axios.get("https://sum-server.100xdevs.com/todos")
-        .then(res => {
-          setTodos(res.data.todos);
-          setLoading(false);
-        });
-      
-      // return () => {
-      //   clearInterval(val);
-      // }
-  }, [/* n */]);
+    const time = setTimeout(() => {
+      setDebounce(input);
+    }, interval);
 
-  return { todos, loading };
+    return () => clearTimeout(time);
+  }, [input,interval]);
+
+  return debounce;
 }
 
 function App() {
-  const {todos, loading} = useTodos();
-
-  if(loading) {
-    return (
-      <div>Loading...</div>
-    )
-  }
+  // const online = useIsOnline();
+  // useInterval(() => {
+    //   setCount(count + 1);
+    // },  1000);
+  // const point = useMousePointer();
+  // const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+  const debouncedValue = useDebounce(inputValue, 500);
 
   return (
     <>
-      {todos.map(todo => <Track todo={todo} />)}
+      {/* {online ? "Online": "Offline"}
+      <br />
+      You are at {point.x}, {point.y}
+      <br />
+      Timer is at {count} */}
+      Debounced value is {debouncedValue}
+      <br />
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder='Search...'/>
     </>
   )
-}
-
-function Track({ todo }) {
-  return <div>
-    {todo.title}
-    <br />
-    {todo.description}
-  </div>
 }
 
 export default App
