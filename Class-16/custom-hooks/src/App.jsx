5d5ec16b811da7a01e0d846b4d130782f48ad57c
AkshatJha21 <akshatj2134@gmail.com>
2024-05-27 20:19:19 +0530

    class based components

--- Class-16/custom-hooks/src/App.jsx
+++ Class-16/custom-hooks/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello
+      <MyComponent />
     </>
   )
+}
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increase</button>
+      </div>
+    )
+  }
 }
 
 export default App
