50a921a940f4df5b7844562217aa39ec95a61d50
AkshatJha21 <akshatj2134@gmail.com>
2024-05-28 09:20:57 +0530

    greet function

---
+++ node-app/a.js
@@ -0,0 +1,7 @@
+"use strict";
+// let x: number = 1;
+// console.log(x);
+function helloWorld(firstName) {
+    console.log(`Hello, ${firstName}`);
+}
+helloWorld("23");
---
+++ node-app/a.ts
@@ -0,0 +1,8 @@
+// let x: number = 1;
+// console.log(x);
+
+function helloWorld(firstName: string) {
+    console.log(`Hello, ${firstName}`);
+}
+
+helloWorld("23");
