4ae9afc140f204a9ebb85a7f899944c4d51a79d5
AkshatJha21 <akshatj2134@gmail.com>
2024-05-28 09:35:30 +0530

    function types

--- node-app/a.ts
+++ node-app/a.ts
@@ -1,15 +1,23 @@
 // let x: number = 1;
 // console.log(x);
 
-function isLegal(age: number): boolean {
-    if (age >= 18) {
-        return true;
-    } else {
-        return false;
-    };
+// function isLegal(age: number): boolean {
+//     if (age >= 18) {
+//         return true;
+//     } else {
+//         return false;
+//     };
+// };
+
+// let x: boolean = isLegal(18);
+// if (x) {
+//     console.log("Adult");
+// }
+
+function runAfterOneSec(fn: () => void) {
+    setTimeout(fn, 1000);
 };
 
-let x: boolean = isLegal(18);
-if (x) {
-    console.log("Adult");
-}
+runAfterOneSec(() => {
+    console.log("Yello There");
+})
