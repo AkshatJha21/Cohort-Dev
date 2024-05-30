71598ec049c6af53cb49469738fe8407b0ba7a70
AkshatJha21 <akshatj2134@gmail.com>
2024-05-30 09:24:44 +0530

    table created

---
+++ postgres/src/index.ts
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+const client = new Client({
+    connectionString: 'postgresql://AkshatJha21:2uB4qKzDrCbl@ep-late-silence-a5zjp6xu.us-east-2.aws.neon.tech/project1-db?sslmode=require'
+});
+
+async function createUsersTable() {
+    await client.connect();
+    const result = await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
+        );
+    `)
+    console.log(result);
+}
+
+createUsersTable();
