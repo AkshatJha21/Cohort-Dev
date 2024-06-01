d0446cbc4b348875443e55eb41a274459a1b4644
AkshatJha21 <akshatj2134@gmail.com>
2024-06-01 13:53:45 +0530

    minor changes

--- Class-19/cloudfare-tutorial/src/index.ts
+++ Class-19/cloudfare-tutorial/src/index.ts
@@ -13,18 +13,8 @@
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		console.log(request.body);
-		console.log(request.headers);
-		console.log(request.method);
-		
-		if (request.method === "GET") {
-			return Response.json({
-				message: "you sent a get request"
-			});
-		} else {
-			return Response.json({
-				message: "you did not send a get request"
-			});
-		}
+		return Response.json({
+			message: "Hello"
+		});
 	},
 };
