79303d8f2e2eb7a28568eab96174378574ae6259
AkshatJha21 <akshatj2134@gmail.com>
2024-08-28 01:16:53 +0530

    new

--- Class-26/next-backend-tutorial/app/api/user/route.ts
+++ Class-26/next-backend-tutorial/app/api/user/route.ts
@@ -5,6 +5,7 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
 
     console.log(body);
+    
 
     return Response.json({
         message: "You are logged in!"
